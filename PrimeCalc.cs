f522f7957af8b44859da482dc19f643e61a6530f
Mikael N <uusikauttaja02022020@outlook.com>
2020-08-25 11:01:10 +0300

    some stuffs

---
+++ PrimeCalc.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace FakeInterview
+{
+    class PrimeCalc
+    {
+        public bool DebugOn { get; set; }
+
+        public PrimeCalc(bool debugOn)
+        {
+            this.DebugOn = debugOn;
+        }
+
+        /// <summary>
+        /// Returns (long) the first prime with n digits
+        /// This is a very slow method
+        /// </summary>
+        public long GetFirstPrimeWithNDigits(int n)
+        {
+            string num = "1".PadRight(n, '0');
+            long l = long.Parse(num);
+            l += 1;
+
+            for (; l <= long.MaxValue ; l+=2)
+            {
+                if (IsPrimePrintTime(l))
+                    return l;
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Returns (bool) if the input is a prime number or not and prints the time to calculate in ms
+        /// </summary>
+        private bool IsPrimePrintTime(long input)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            bool isPrime = IsPrime(input);
+            sw.Stop();
+
+            if (DebugOn)
+                Console.WriteLine($"Time: {sw.ElapsedMilliseconds}ms for value {input}. isPrime = {isPrime}");
+
+            return isPrime;
+        }
+
+        /// <summary>
+        /// Returns (bool) if the input is a prime number or not
+        /// </summary>
+        public bool IsPrime(long input)
+        {
+            long l = 1;
+
+            for (; l < input; l+=2)
+            {
+                if (input % l == 0 && l != 1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace FakeInterview
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PrimeCalc primeCalc = new PrimeCalc(true);
+
+            long a = primeCalc.GetFirstPrimeWithNDigits(10);
+            Console.WriteLine($"a: {a}");
+
+            long b = primeCalc.GetFirstPrimeWithNDigits(11);
+            Console.WriteLine($"a: {b}");
+
+            long result = a * b;
+
+            Console.WriteLine($"Result: {a} * {b} =  {result}");
+
+            string output = result.ToString();
+            int outputLen = output.Length;
+            int startIndex = outputLen - 15;
+
+            Console.WriteLine($"Last 15 digits = {output.Substring(startIndex)}");
         }
     }
 }
