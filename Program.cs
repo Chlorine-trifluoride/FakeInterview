70b1dd9af88e8b3777a4c91d4c12cf3254f11773
Mikael N <uusikauttaja02022020@outlook.com>
2020-08-25 10:10:05 +0300

    init project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FakeInterview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
